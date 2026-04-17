789d7524df33cee803a6ebc1df2d92a998f129cb
Abdullah Al Jubair <jubair2810@gmail.com>
2026-04-17 15:05:40 +0600

    added build

--- sneaker-backend/src/app/module/products/product.controller.ts
+++ sneaker-backend/src/app/module/products/product.controller.ts
@@ -3,22 +3,10 @@ import status from "http-status";
 import catchAsync from "../../../utils/catchAsyn";
 import sendResponse from "../../../utils/sendResponse";
 import {productServices} from "./product.servivce";
-import { broadcastProductUpdate } from "../../../socket";
 
 const createProduct = catchAsync(async (req: Request, res: Response) => {
   const result = await productServices.createProduct(req.body);
-  
-  // Broadcast product update to all connected clients
-  try {
-    broadcastProductUpdate({
-      action: 'created',
-      product: result,
-      timestamp: new Date(),
-    });
-  } catch (error) {
-    console.log('Socket broadcast failed:', error);
-  }
-  
+
   sendResponse(res, {
     success: true,
     statusCode: status.OK,
