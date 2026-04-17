d58332f4669e0626acea998c48ad863464dda484
Abdullah Al Jubair <jubair2810@gmail.com>
2026-04-17 10:35:36 +0600

    added shcema with relation

--- sneaker-backend/prisma.config.ts
+++ sneaker-backend/prisma.config.ts
@@ -1,4 +1,8 @@
-import {defineConfig, env} from "prisma/config";
+import { defineConfig } from "prisma/config";
+import { config } from "dotenv";
+
+
+config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -6,6 +10,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env["DATABASE_URL"] as string,
   },
 });
